260f7d08fa3dd302b2b017e9d348f6dbc40a7d90
Mike McGuire <MoonshotMike@gmail.com>
2017-01-01 10:36:46 -0700

    Office Commit Test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MEANTodos is ready';
+  title = 'MEANTodos is ready - Office Commit';
 }
