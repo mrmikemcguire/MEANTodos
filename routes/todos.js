4419c48cd658ffaafcea3c9c6668d13be6176336
Mike McGuire <MoonshotMike@gmail.com>
2017-01-04 08:58:11 -0700

    lecture 22 completed

--- routes/todos.js
+++ routes/todos.js
@@ -30,4 +30,71 @@ router.get('/todo/:id', function (req, res, next) {
     });
 });
 
+//Save Todo
+router.post('/todo', function (req, res, next) {
+    var todo = req.body;
+    if(!todo.text || !(todo.isCompleted + '')) {
+        res.status(400);
+        res.json({
+            "error": "Invalid Data"
+        });
+    }
+    else {
+        db.save(todo, function (err, result) {
+            if(err){
+                res.send(err);
+            }
+            else{
+                res.json(result);
+            }
+        });
+    }
+
+});
+
+//Update ToDo
+router.put('/todo/:id', function (req, res, next) {
+    var todo = req.body;
+    var updatedObj = {};
+        if(todo.isCompleted){
+          updatedObj.isCompleted = todo.isCompleted;
+        }
+        if(todo.text){
+            updatedObj.text = todo.text;
+        }
+        if(!updatedObj){
+            res.status(400);
+            res.json({
+                "error": "Invalid Data"
+            });
+        }
+        else{
+            db.todos.update({
+                _id: mongojs.ObjectId(req.params.id)
+            }, updatedObj, {}, function (err, result) {
+                if(err){
+                    res.send(err);
+                }
+                else{
+                    res.json(result);
+                }
+            });
+        }
+
+});
+
+//Delete ToDo
+router.delete('/todo/:id', function (req, res, next) {
+        db.todos.remove({
+            _id: mongojs.ObjectId(req.params.id)
+        }, '', function (err, result) {
+            if(err){
+                res.send(err);
+            }
+            else{
+                res.json(result);
+            }
+        });
+});
+
 module.exports = router;
