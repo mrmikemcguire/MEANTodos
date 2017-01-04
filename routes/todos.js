faa91b750d3f1684456c2d243bcc6bb7736cd3f3
Mike McGuire <MoonshotMike@gmail.com>
2017-01-04 08:38:52 -0700

    mid lecture 22

--- routes/todos.js
+++ routes/todos.js
@@ -1,8 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://cw:aikido@ds155028.mlab.com:55028/meantodos_udemy_12apps', ['todos']);
 
+//Get Todos
 router.get('/', function (req, res, next) {
-    res.send('TODOS API');
+    db.todos.find(function (err, todos) {
+        if(err){
+            res.send(err);
+        }
+        else{
+            res.json(todos);
+        }
+
+    });
+});
+
+//Get Single Todo
+router.get('/todo/:id', function (req, res, next) {
+    db.todos.findOne({
+        _id: mongojs.ObjectId(req.params.id)
+    }, function (err, todo) {
+            if(err){
+                res.send(err);
+            }
+            else{
+                res.json(todo);
+            }
+    });
 });
 
 module.exports = router;
